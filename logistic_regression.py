f383c723025d2c16bb8497c8c2e22027bbf9a661
shntm <shantam.agarwal95@gmail.com>
2017-09-17 21:35:49 +0530

    Logisitc Regr with two features

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,14 +4,20 @@ import matplotlib.pyplot as plt
 import numpy
 data = datasets.load_breast_cancer()
 radiusData = data.data[:,0]
+areaData = data.data[:,3]
 targetData = data.target
 radiusInput = numpy.reshape(radiusData[:-50], (-1,1))
+areaInput = numpy.reshape(areaData[:-50], (-1,1))
+combinedInput = numpy.concatenate((radiusInput, areaInput),axis=1)
 testRadiusData =  numpy.reshape(radiusData[-50:], (-1,1))
+testAreaData =  numpy.reshape(areaData[-50:], (-1,1))
+combinedTest = numpy.concatenate((testRadiusData, testAreaData),axis=1)
+print(combinedTest)
 targetInput = targetData[:-50]
 testTargetData = targetData[-50:]
 model = linear_model.LogisticRegression()
-model.fit(radiusInput, targetInput)
-predOutput = model.predict(testRadiusData)
+model.fit(combinedInput, targetInput)
+predOutput = model.predict(combinedTest)
 print(predOutput)
 print(testTargetData)
 print(metrics.classification_report(testTargetData, predOutput))
