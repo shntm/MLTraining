d26f8a05ad46520c70eeeb25b03a851e08a15766
Shantam Agarwal <shantam.agarwal@kony.com>
2017-09-18 12:22:18 +0530

    Explained code for future ref

--- linear_regression.py
+++ linear_regression.py
@@ -3,27 +3,52 @@ import numpy as numpy
 from sklearn.metrics import mean_squared_error
 # import matplotlib.pyplot as plotter
 
+# load the diabetes dataset
 diabetes = datasets.load_diabetes()
 bmiList = []
 bpList = []
 targetList = []
+
+#add feature data to respective list
 for i in range(len(diabetes.data)):
 	bmiList.append(diabetes.data[i][2])
 	bpList.append(diabetes.data[i][3])
 	targetList.append(diabetes.target[i])
+
+#reshape lists to numpy vector array also keep 5 inputs for samples accuracy
 bmiTestInput = numpy.reshape(bmiList[:-5],(-1,1))
 bpTestInput = numpy.reshape(bpList[:-5],(-1,1))
+
+#inputMatrix for BP and BMI features combined
 inputMatrix = numpy.concatenate((bmiTestInput,bpTestInput), axis=1)
+
+#prediction inputs for BP and BMI features separately
 bmiPredInput = numpy.reshape(bmiList[-5:],(-1,1))
 bpPredInput = numpy.reshape(bpList[-5:],(-1,1))
+
+#prediction inputs for BP and BMI features combined
 predInputMatrix = numpy.concatenate((bmiPredInput,bpPredInput), axis=1)
+
+#actual prediction outputs to test accuracy
 testOut = numpy.reshape(targetList[:-5],(-1,1))
+
+#load the model
 regr = linear_model.LinearRegression()
 regr2 = linear_model.LinearRegression();
+
+#fit  model with just BMI
 regr2.fit(bmiTestInput, targetList[:-5])
+
+#fit  model with both features
 regr.fit(inputMatrix,targetList[:-5])
+
+#model's prediction for both features combined
 finalPredCombined = regr.predict(predInputMatrix)
+
+#model's prediction for only BMI inputs
 finalPredBMI = regr2.predict(bmiPredInput)
+
+#mean squared errors for both predictions for comparision
 print("Mean squared error for combined: %.2f" % mean_squared_error(targetList[-5:], finalPredCombined))
 print("Mean squared error for BMI: %.2f" % mean_squared_error(targetList[-5:], finalPredBMI))
 # plotter.scatter(bpList, targetList)
--- logistic_regression.py
+++ logistic_regression.py
@@ -2,24 +2,35 @@ from sklearn import datasets, metrics
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 import numpy
+
+#load breast cancer dataset
 data = datasets.load_breast_cancer()
+
+#load features into respective arrays
 radiusData = data.data[:,0]
 areaData = data.data[:,3]
 targetData = data.target
+
+#reshape features for sklearn consumption also segregate 50 samples to test accuracy
 radiusInput = numpy.reshape(radiusData[:-50], (-1,1))
 areaInput = numpy.reshape(areaData[:-50], (-1,1))
 combinedInput = numpy.concatenate((radiusInput, areaInput),axis=1)
 testRadiusData =  numpy.reshape(radiusData[-50:], (-1,1))
 testAreaData =  numpy.reshape(areaData[-50:], (-1,1))
 combinedTest = numpy.concatenate((testRadiusData, testAreaData),axis=1)
-print(combinedTest)
 targetInput = targetData[:-50]
 testTargetData = targetData[-50:]
+
+#load model
 model = linear_model.LogisticRegression()
 model.fit(combinedInput, targetInput)
 predOutput = model.predict(combinedTest)
+
+#final prediction vs actual outputs for comparision
 print(predOutput)
 print(testTargetData)
+
+#reports to measure accuracy
 print(metrics.classification_report(testTargetData, predOutput))
 print(metrics.confusion_matrix(testTargetData, predOutput))
 # plt.scatter(radiusData, targetData)
