c9bbeeace562b631b2227e4bd5c1723478efac35
Shantam Agarwal <shantam.agarwal@kony.com>
2017-09-21 12:45:07 +0530

    comments for future ref

--- decision_trees_titanic.py
+++ decision_trees_titanic.py
@@ -2,16 +2,29 @@ import pandas as pd
 import numpy as np
 from sklearn import tree, preprocessing, metrics, linear_model
 import graphviz
-samplesToTest = 100
+
+#defining samples to test with
+samplesToTest = 200
 samplesToTest = -1*samplesToTest
+
+#load dataset from CSV
 dataset = pd.read_csv('train-1.csv')
+
+#load feature names to list
 feature_names = list(dataset)
+
+#filling empty spots. Must impute or remove later
 dataset["Embarked"] = dataset["Embarked"].fillna('0')
 dataset["Age"] = dataset["Age"].fillna(-1)
-dataset.describe();
+
+#converting dataset to numpy array
 datasetMatrix = dataset.as_matrix()
 datasetArr = np.array(datasetMatrix)
+
+#defining target data i.e. survival of passenger
 targetData = np.asarray(datasetArr[:,1], dtype=np.float64)
+
+#label encoding for string values
 targetClassName = ['died', 'survived']
 embarkPointArray = ['S', 'Q', 'C', '0']
 genderArray = ['male', 'female']
@@ -21,34 +34,40 @@ lblEncoder = preprocessing.LabelEncoder()
 lblEncoderForGender = preprocessing.LabelEncoder()
 lblEncoder.fit(embarkPointArray)
 lblEncoderForGender.fit(genderArray)
-# print(usableData[:,2])
 usableData[:,-1] = lblEncoder.transform(usableData[:,-1]);
 usableData[:,1] = lblEncoderForGender.transform(usableData[:,1])
-# print(usableData[:,1])
-# print(lblEncoderForGender.inverse_transform(usableData[:,1].astype(int)))
+
+#all sample values to float
 usableData = np.asarray(usableData, dtype=np.float64)
-# print(usableData)
+
+#dividing to training and test data
 trainingData = usableData[:samplesToTest,:]
 testData = usableData[samplesToTest:,:]
 targetTrainingData = targetData[:samplesToTest]
 targetTestData = targetData[samplesToTest:]
 
-print(featureNames)
+#Decision Tree classifier prediction
 clftree = tree.DecisionTreeClassifier()
 clftree.fit(trainingData, targetTrainingData)
 predictionDataTree = clftree.predict(testData)
 
+#Logistic regression classifier prediction
 clrLoRegr = linear_model.LogisticRegression()
 clrLoRegr.fit(trainingData, targetTrainingData)
 predictionDateLoRegr = clrLoRegr.predict(testData)
 
+#predictions and target data
 print(predictionDataTree)
 print(predictionDateLoRegr)
 print(targetTestData)
+
+#metrics for comparision between both classifiers
 print(metrics.accuracy_score(targetTestData,predictionDataTree))
 print(metrics.confusion_matrix(targetTestData, predictionDataTree))
 print(metrics.accuracy_score(targetTestData,predictionDateLoRegr))
 print(metrics.confusion_matrix(targetTestData, predictionDateLoRegr))
+
+#export decision tree to pdf
 # dot_data = tree.export_graphviz(clf, out_file=None, feature_names = featureNames)
 # graph = graphviz.Source(dot_data)
 # graph.render("titanic_tree")
